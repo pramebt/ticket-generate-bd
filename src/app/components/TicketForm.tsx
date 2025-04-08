0a57746f981c4667de6a6c691d9618fc5587626b
prame BT <pramebt@gmail.com>
2025-04-08 22:43:34 +0700

    add bg and header

--- src/app/components/TicketForm.tsx
+++ src/app/components/TicketForm.tsx
@@ -1,6 +1,22 @@
 const TicketForm = () => {
+  
   return (
-    <div>TicketForm</div>
-  )
-}
-export default TicketForm
+    <div className="">
+      {/* Head */}
+      <div className="flex flex-col justify-center pt-5 text-center">
+        <h1 className="text-white font-bold text-[52px]">
+          Your Journey to Coding Conf <br />
+          2025 Starts Here!{" "}
+        </h1>
+        <p className="text-gray-400">
+          Secure your spot at next year's biggest codeing conference.
+        </p>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+  );
+};
+export default TicketForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,39 @@
-import TicketForm from "./components/TicketForm"
-
+import Image from "next/image";
+import TicketForm from "./components/TicketForm";
 
 const page = () => {
   return (
-<div
-  className="
+    <div
+      className="
     bg-[url('/assets/images/background-mobile.png')]   // default: mobile
     sm:bg-[url('/assets/images/background-tablet.png')] // ≥640px
     md:bg-[url('/assets/images/background-desktop.png')] // ≥768px
-    bg-cover bg-center w-screen h-screen
+    bg-cover bg-center w-screen h-screen 
   "
->
-  <div className="">
+    >
+      {/* <div
+        className="
+  bg-[url('/assets/images/pattern-squiggly-line-bottom-mobile-tablet.svg')]
+  md:bg-[url('/assets/images/pattern-squiggly-line-bottom-desktop.svg')]
+  bg-left-bottom bg-no-repeat
+  bg-[length:200px] md:bg-[length:400px] lg:bg-[length:800px]
+  w-screen h-screen
+"
+      > */}
+        
+          <div className="flex justify-center pt-5 ">
+            <Image
+              src="/assets/images/logo-full.svg"
+              alt=""
+              width={100}
+              height={100}
+              className="w-30"
+            />
+          </div>
+          <TicketForm />
+        </div>
+      
     
-  <TicketForm/>
-  </div>
-</div>
-  )
-}
-export default page
+  );
+};
+export default page;
